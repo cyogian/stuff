8d9777b5534bf7ff4d48f142d41468eb9243580e
Amar Nath Yogi <immortal2907@gmail.com>
2021-10-27 15:08:52 +0530

    Update Results.js

--- Results.js
+++ Results.js
@@ -14,7 +14,7 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
+import SearchIcon from "@material-ui/icons/Search"; 
 import ZoomInIcon from "@material-ui/icons/ZoomIn";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
@@ -33,259 +33,266 @@ import {
   viewDocument,
 } from "../../../redux/slices/docSlice";
 import { PDFFindController } from "pdfjs-dist/web/pdf_viewer";
-import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
-const useStyles = makeStyles((theme) => ({
-  container: {
-    height: "100%",
-    display: "flex",
-    padding: "0.5rem",
-    "& > div": {
-      width: "50%",
-      height: "calc(100% - 1rem)",
-      borderRadius: "16px",
-      margin: "0.5rem",
-      padding: "1rem",
-    },
-  },
-  tableContainer: {
-    background: `${theme.palette.common.stride_light_white}`,
-  },
-  viewerContainer: {
-    background: `${theme.palette.common.stride_light_black}`,
-  },
-  tableBox: {
-    height: "calc(100% - 3rem)",
-    marginTop: "1.5rem",
-    overflow: "auto",
-  },
-  strideH2Medium: {
-    ...theme.typography.stride_h2_medium,
-  },
-  dpNo: {
-    ...theme.typography.stride_h3_medium,
-    color: "black",
-  },
-  tableHead: {
-    ...theme.typography.stride_h3_medium,
-    background: `${theme.palette.common.stride_light_black}`,
-    position: "sticky",
-    top: "0",
-  },
-  headCell: {
-    color: `${theme.palette.common.stride_light_white}`,
-    padding: "0.5rem",
-  },
-  strideBodyRegular: {
-    ...theme.typography.stride_body_regular,
-  },
-  strideBodyLight16: {
-    ...theme.typography.stride_body_light_16pt,
-  },
-  bbox: {
-    cursor: "pointer",
-    color: `${theme.palette.common.stride_iris}`,
-  },
-  check: {
-    padding: "0 8px 0 0",
-  },
-  search: {
-    paddingLeft: "1rem",
-    maxWidth: "240px",
-    textAlign: "center",
-    position: "relative",
-  },
-  searchRoot: {
-    color: "#DDDDDD",
-    borderRadius: "8px",
-    border: "1px solid #DDDDDD",
-    "&:before": {
-      borderBottom: "none !important",
-      transition: "none !important",
-    },
-    "&:after": {
-      borderBottom: "none !important",
-      transition: "none !important",
-    },
-    maxWidth: "12rem",
-  },
-  searchNav: {
-    position: "absolute",
-    left: "calc(50% - 28px)",
-    top: "100%",
-    zIndex: "10",
-    background: "#333333",
-    borderBottomLeftRadius: "8px",
-    borderBottomRightRadius: "8px",
-    color: "#dddddd",
-    "& > button": {
+  import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      height: "100%",
+      display: "flex",
       padding: "0.5rem",
+      "& > div": {
+        width: "50%",
+        height: "calc(100% - 1rem)",
+        borderRadius: "16px",
+        margin: "0.5rem",
+        padding: "1rem",
+      },
     },
-  },
-  pageInput: {
-    background: "none",
-    textAlign: "right",
-    fontSize: "inherit",
-    fontWeight: "inherit",
-    outline: "none",
-    color: "#DDDDDD",
-    "&::-webkit-outer-spin-button": {
-      "-webkit-appearance": "none",
-      margin: 0,
+    tableContainer: {
+      background: `${theme.palette.common.stride_light_white}`,
     },
-    "&::-webkit-inner-spin-button": {
-      "-webkit-appearance": "none",
-      margin: 0,
+    viewerContainer: {
+      background: `${theme.palette.common.stride_light_black}`,
     },
-    "-moz-appearance": "textfield",
-    boxShadow: "none",
-    border: "none",
-    borderBottom: "1px solid #DDDDDD",
-  },
-  scaleSelect: {
-    background: "none",
-    border: "none",
-    color: "#DDDDDD",
-    "& > option": {
+    tableBox: {
+      height: "calc(100% - 3rem)",
+      marginTop: "1.5rem",
+      overflow: "auto",
+    },
+    strideH2Medium: {
+      ...theme.typography.stride_h2_medium,
+    },
+    dpNo: {
+      ...theme.typography.stride_h3_medium,
       color: "black",
     },
-    outline: "none",
-    textAlign: "center",
-  },
-}));
-
-export default function Results(props) {
-  const classes = useStyles();
-  const [highlights, setHighlights] = useState([]);
-  const docId = props.match.params.docId.trim();
-  const doc = useSelector(selectDocument);
-  const docLoading = useSelector(selectDocumentLoading);
-  const docError = useSelector(selectDocumentError);
-  const dispatch = useDispatch();
-  const [page, setPage] = useState(0);
-  const [pageInput, setPageInput] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
-  const [searchField, setSearchField] = useState("");
-  const [scale, setScale] = useState("auto");
-  useEffect(() => {
-    docId && dispatch(viewDocument(docId));
-  }, [docId, dispatch]);
-  useEffect(() => {
-    setPageInput(page);
-  }, [page]);
-  const bboxToHighlight = (obj) => {
-    const [x1, _y1, x2, _y2] = obj.bbox;
-    const [px1, py1, px2, py2] = obj.page_bbox;
-    const width = Math.abs(px2 - px1);
-    const height = Math.abs(py2 - py1);
-    const y2 = height - _y1;
-    const y1 = height - _y2;
-    return {
-      content: {
-        image: "",
+    tableHead: {
+      ...theme.typography.stride_h3_medium,
+      background: `${theme.palette.common.stride_light_black}`,
+      position: "sticky",
+      top: "0",
+    },
+    headCell: {
+      color: `${theme.palette.common.stride_light_white}`,
+      padding: "0.5rem",
+    },
+    strideBodyRegular: {
+      ...theme.typography.stride_body_regular,
+    },
+    strideBodyLight16: {
+      ...theme.typography.stride_body_light_16pt,
+    },
+    bbox: {
+      cursor: "pointer",
+      color: `${theme.palette.common.stride_iris}`,
+    },
+    check: {
+      padding: "0 8px 0 0",
+    },
+    search: {
+      paddingLeft: "1rem",
+      maxWidth: "240px",
+      textAlign: "center",
+      position: "relative",
+    },
+    searchRoot: {
+      color: "#DDDDDD",
+      borderRadius: "8px",
+      border: "1px solid #DDDDDD",
+      "&:before": {
+        borderBottom: "none !important",
+        transition: "none !important",
       },
-      position: {
-        boundingRect: {
-          x1,
-          y1,
-          x2,
-          y2,
-          width,
-          height,
+      "&:after": {
+        borderBottom: "none !important",
+        transition: "none !important",
+      },
+      maxWidth: "12rem",
+    },
+    searchNav: {
+      position: "absolute",
+      left: "calc(50% - 28px)",
+      top: "100%",
+      zIndex: "10",
+      background: "#333333",
+      borderBottomLeftRadius: "8px",
+      borderBottomRightRadius: "8px",
+      color: "#dddddd",
+      "& > button": {
+        padding: "0.5rem",
+      },
+    },
+    pageInput: {
+      background: "none",
+      textAlign: "right",
+      fontSize: "inherit",
+      fontWeight: "inherit",
+      outline: "none",
+      color: "#DDDDDD",
+      "&::-webkit-outer-spin-button": {
+        "-webkit-appearance": "none",
+        margin: 0,
+      },
+      "&::-webkit-inner-spin-button": {
+        "-webkit-appearance": "none",
+        margin: 0,
+      },
+      "-moz-appearance": "textfield",
+      boxShadow: "none",
+      border: "none",
+      borderBottom: "1px solid #DDDDDD",
+    },
+    scaleSelect: {
+      background: "none",
+      border: "none",
+      color: "#DDDDDD",
+      "& > option": {
+        color: "black",
+      },
+      outline: "none",
+      textAlign: "center",
+    },
+  }));
+
+  export default function Results(props) {
+    const classes = useStyles();
+    const [highlights, setHighlights] = useState([]); // Array Of Annotations
+    const docId = props.match.params.docId.trim();
+    const doc = useSelector(selectDocument);
+    const docLoading = useSelector(selectDocumentLoading);
+    const docError = useSelector(selectDocumentError);
+    const dispatch = useDispatch();
+    const [page, setPage] = useState(0);
+    const [pageInput, setPageInput] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [searchField, setSearchField] = useState("");
+    const [scale, setScale] = useState("auto");
+    useEffect(() => {
+      docId && dispatch(viewDocument(docId));
+    }, [docId, dispatch]);
+    useEffect(() => {
+  //     set page input on page switch through scroll or navigation buttons
+      setPageInput(page);
+    }, [page]);
+    const bboxToHighlight = (obj) => {
+  //     Stride BBOX object to react-pdf-highlighter higlight object
+      const [x1, _y1, x2, _y2] = obj.bbox;
+      const [px1, py1, px2, py2] = obj.page_bbox;
+      const width = Math.abs(px2 - px1);
+      const height = Math.abs(py2 - py1);
+      const y2 = height - _y1;
+      const y1 = height - _y2;
+      return {
+        content: {
+          image: "",
         },
-        rects: [],
-        pageNumber: obj.page_no + 1,
-        id: "currentHighlight",
-      },
+        position: {
+          boundingRect: {
+            x1,
+            y1,
+            x2,
+            y2,
+            width,
+            height,
+          },
+          rects: [],
+          pageNumber: obj.page_no + 1,
+          id: "currentHighlight",
+        },
+      };
     };
-  };
-  const highlightToBbox = (position) => {
-    const {
-      pageNumber,
-      boundingRect: { x1, y1, x2, y2, width, height },
-    } = position;
-    return {
-      bbox: [x1, height - y2, x2, height - y1],
-      page_no: pageNumber - 1,
-      page_bbox: [0, 0, width, height],
+    const highlightToBbox = (position) => {
+      // react-pdf-highlighter position object(onAreaSelection) to Stride BBOX object 
+      const {
+        pageNumber,
+        boundingRect: { x1, y1, x2, y2, width, height },
+      } = position;
+      return {
+        bbox: [x1, height - y2, x2, height - y1],
+        page_no: pageNumber - 1,
+        page_bbox: [0, 0, width, height],
+      };
     };
-  };
-  const pdfHighlighter = useRef(null);
-  const scrollToHighlight = useCallback(() => {
-    const highlight = highlights[0];
-    if (highlight) {
-      pdfHighlighter.current?.scrollTo(highlight);
-    }
-  }, [highlights, pdfHighlighter]);
+    const pdfHighlighter = useRef(null); // ref for current pdf highlighter object
+    const scrollToHighlight = useCallback(() => {
+      // scrollToHighlight callback 
+      const highlight = highlights[0];
+      if (highlight) {
+        pdfHighlighter.current?.scrollTo(highlight);
+      }
+    }, [highlights, pdfHighlighter]);
 
-  useEffect(() => {
-    scrollToHighlight();
-  }, [scrollToHighlight]);
+    useEffect(() => {
+      scrollToHighlight();
+    }, [scrollToHighlight]);
 
-  const onChangeHighlight = (obj) => {
-    setHighlights([bboxToHighlight(obj)]);
-  };
+    const onChangeHighlight = (obj) => {
+      // runs whwn a new highlight/annotation is selected
+      setHighlights([bboxToHighlight(obj)]);
+    };
 
-  const bboxList = (bbox_objects = [], name = "") => {
-    const dps = bbox_objects.map((obj, i) => {
+    const bboxList = (bbox_objects = [], name = "") => {
+      const dps = bbox_objects.map((obj, i) => {
+        return (
+          <Typography
+            key={`${name}_${obj.dp_value}_${i}`}
+            onClick={(e) => {
+              onChangeHighlight(obj);
+            }}
+            className={classes.bbox}
+          >
+            {obj.text}
+          </Typography>
+        );
+      });
       return (
-        <Typography
-          key={`${name}_${obj.dp_value}_${i}`}
-          onClick={(e) => {
-            onChangeHighlight(obj);
-          }}
-          className={classes.bbox}
-        >
-          {obj.text}
-        </Typography>
+        <div style={{ display: "flex", alignItems: "flex-start" }}>
+          <Checkbox
+            disableRipple
+            color="primary"
+            classes={{ root: classes.check }}
+          />
+          <div>{dps}</div>
+        </div>
       );
-    });
-    return (
-      <div style={{ display: "flex", alignItems: "flex-start" }}>
-        <Checkbox
-          disableRipple
-          color="primary"
-          classes={{ root: classes.check }}
-        />
-        <div>{dps}</div>
-      </div>
-    );
-  };
-  const tableBody = ({ file_name, ...dataPoints }) => {
-    const list = Object.keys(dataPoints).map((k) => {
-      const dp = dataPoints[k];
-      return (
-        <TableRow
-          key={`datapoint:${k}:${dp.dp_name}`}
-          style={{ verticalAlign: "top" }}
-        >
-          <TableCell>
-            <Typography className={classes.dpNo}>{Number(k) + 1}.</Typography>
-          </TableCell>
-          <TableCell>
-            <Typography key={`${dp.dp_name}`} className={classes.dpNo}>
-              {dp.dp_name}
-            </Typography>
-          </TableCell>
-          <TableCell>{bboxList(dp.bbox_objects, dp.dp_name)}</TableCell>
-        </TableRow>
-      );
+    };
+    const tableBody = ({ file_name, ...dataPoints }) => {
+      // table of Annotations
+      const list = Object.keys(dataPoints).map((k) => {
+        const dp = dataPoints[k];
+        return (
+          <TableRow
+            key={`datapoint:${k}:${dp.dp_name}`}
+            style={{ verticalAlign: "top" }}
+          >
+            <TableCell>
+              <Typography className={classes.dpNo}>{Number(k) + 1}.</Typography>
+            </TableCell>
+            <TableCell>
+              <Typography key={`${dp.dp_name}`} className={classes.dpNo}>
+                {dp.dp_name}
+              </Typography>
+            </TableCell>
+            <TableCell>{bboxList(dp.bbox_objects, dp.dp_name)}</TableCell>
+          </TableRow>
+        );
     });
     return <TableBody>{list}</TableBody>;
   };
   const onPageChange = (evt) => {
+  // runs on page change
     setPage(evt.pageNumber);
   };
   const onPageInputChange = (evt) => {
     if (totalPages >= evt.target.value >= 1) {
       document
         .querySelector(`[data-page-number="${evt.target.value}"]`)
-        ?.scrollIntoView();
+        ?.scrollIntoView(); // jumping to new page by using data-page-number attribute selector
     } else {
       setPageInput(page);
     }
   };
-  const [pdfDocument, setPdfDocument] = useState(null);
-  const [pdfFindController, setPdfFindController] = useState(null);
+  const [pdfDocument, setPdfDocument] = useState(null); // stores pdf document object
+  const [pdfFindController, setPdfFindController] = useState(null); // stores pdf find controller object that controlls search (you can refer to pdfJS)
 
   // Initializing PDFViewer
   useEffect(() => {
